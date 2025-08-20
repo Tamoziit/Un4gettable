76638c32aa4cf4c0fb87ba81b188b95be5d3c8aa
Tamojit Das <tamojitdas181007@gmail.com>
2025-08-21 01:57:15 +0530

    SMS SOS service

--- backend/src/routes/admin.routes.ts
+++ backend/src/routes/admin.routes.ts
@@ -1,8 +1,11 @@
 import express from 'express';
-import { getToken } from '../controllers/admin.controller';
+import { getToken, sendSMS } from '../controllers/admin.controller';
+import verifyAdmin from '../middlewares/admin.middleware';
 
 const router = express.Router();
 
 router.post("/get-token", getToken);
+router.post("/send-sms", verifyAdmin, sendSMS);
+//router.post("/send-sms-2", verifyAdmin, sendSMS2);
 
 export default router;
