a555ab3695ef2a5d1d5deacae28ff0d3f11496d3
Tamojit Das <tamojitdas181007@gmail.com>
2025-08-21 12:02:17 +0530

    Govt. pick a problem

--- backend/src/routes/govt.routes/problem.routes.ts
+++ backend/src/routes/govt.routes/problem.routes.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import verifyGovt from "../../middlewares/govtAuth.middleware";
-import { viewProblemById, viewProblems } from "../../controllers/govt.controllers/problem.controller";
+import { pickAProblem, viewProblemById, viewProblems } from "../../controllers/govt.controllers/problem.controller";
 
 const router = express.Router();
 
 router.get("/problem-repository", verifyGovt, viewProblems);
 router.get("/problem-repository/:id", verifyGovt, viewProblemById);
+router.patch("/pick-a-problem/:id", verifyGovt, pickAProblem);
 
 export default router;
