9fa524fec94edf59e4df7ea0674f4702544b7b28
Tamojit Das <tamojitdas181007@gmail.com>
2025-09-06 15:52:34 +0530

    Dashboard hotspots UI: 2

--- android/components/dashboard/CriticalAlert.tsx
+++ android/components/dashboard/CriticalAlert.tsx
@@ -1,10 +1,14 @@
+import { StateDataProps } from '@/interfaces/interfaces';
 import { View, Text } from 'react-native';
 
-const CriticalAlert = () => {
+const CriticalAlert = ({ stateData }: StateDataProps) => {
   return (
-    <View>
-      <Text>CriticalAlert</Text>
-    </View>
+    <View className="bg-red-900/30 border border-red-500 rounded-lg p-4 w-full">
+			<Text className="text-red-400 font-bold mb-2 text-lg">🚨 Critical Alert</Text>
+			<Text className="text text-red-200">
+				{stateData[0]?.state} shows the highest deforestation rate with {stateData[0]?.loss_ha.toFixed(1)} ha lost.
+			</Text>
+		</View>
   )
 }
 
